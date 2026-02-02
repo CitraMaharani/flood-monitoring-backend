b02546c0971c7c60de5015b4b25d233ba94f9772
Citra Maharani <citramaharani@students.undip.ac.id>
2026-02-02 16:34:10 +0700

    change port

--- migrations/20260202092323-create-users.js
+++ migrations/20260202092323-create-users.js
@@ -1,22 +1,38 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      role: {
+        type: Sequelize.ENUM('admin', 'user'),
+        defaultValue: 'user'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
   }
 };
--- seeders/20260202092455-seed-users.js
+++ seeders/20260202092455-seed-users.js
@@ -1,25 +1,20 @@
 'use strict';
+const bcrypt = require('bcrypt');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        username: 'admin',
+        password: await bcrypt.hash('admin123', 10),
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
