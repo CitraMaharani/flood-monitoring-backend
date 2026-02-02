9d54409000d554cf110f17129d7824c8d547822a
Citra Maharani <citramaharani@students.undip.ac.id>
2026-02-02 17:56:29 +0700

    fix seeders water level

--- seeders/20260202101334-seed-water-levels.js
+++ seeders/20260202101334-seed-water-levels.js
@@ -3,14 +3,15 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('water_levels', [
-      {
-        sensor_id: 'sensor01',
-        distance_cm: 263.76,
-        timestamp: new Date(),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
+      { id: 2, sensor_id: 'sensor01', distance_cm: 263.76, timestamp: '2025-08-03 23:06:26', createdAt: new Date(), updatedAt: new Date() },
+      { id: 3, sensor_id: 'sensor01', distance_cm: 263.36, timestamp: '2025-08-03 23:06:36', createdAt: new Date(), updatedAt: new Date() },
+      { id: 4, sensor_id: 'sensor01', distance_cm: 263.35, timestamp: '2025-08-03 23:06:55', createdAt: new Date(), updatedAt: new Date() },
+      // lanjutkan semua sampai id 41 (copy persis dari SQL dump)
     ]);
+
+    await queryInterface.sequelize.query(
+      `ALTER TABLE water_levels AUTO_INCREMENT = 42;`
+    );
   },
 
   async down(queryInterface) {
